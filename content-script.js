9f2ee546d6825901e16df1552eef2d43d4ddebcf
Pacifiky <87620151+infinitypacific@users.noreply.github.com>
2024-01-07 17:25:34 -0700

    Formatted

--- content-script.js
+++ content-script.js
@@ -1,16 +1,16 @@
 "use strict"
-  function setimages() {
-    let images = document.querySelectorAll('img');
-    if(images){
-      for(let i = 0; i < images.length; i++){
-          images[i].style = "pointer-events: auto;";
-      };
+function setimages() {
+  let images = document.querySelectorAll('img');
+  if (images) {
+    for (let i = 0; i < images.length; i++) {
+      images[i].style = "pointer-events: auto;";
     };
   };
-  setimages();
-  const imgdetector = new MutationObserver(setimages);
-  imgdetector.observe(document.body, {
-    childList: true,
+};
+setimages();
+const imgdetector = new MutationObserver(setimages);
+imgdetector.observe(document.body, {
+  childList: true,
   subtree: true
 });
 console.log("loaded");
