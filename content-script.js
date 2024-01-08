92925ea88aea3758350456540c4e96a36285aefc
Pacifiky <87620151+infinitypacific@users.noreply.github.com>
2024-01-07 17:17:54 -0700

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,12 +1,15 @@
-const imgdetector = new MutationObserver(mutations => {
-  let images = document.querySelector('img');
-  if(images){
-    for(let i = 0; i < images.length; i++){
-      images[i].style = "pointer-events: auto;";
+document.addEventListener("DOMContentLoaded", (event) => {
+  const imgdetector = new MutationObserver(mutations => {
+    let images = document.querySelector('img');
+    if(images){
+      for(let i = 0; i < images.length; i++){
+          images[i].style = "pointer-events: auto;";
+      }
     }
-  }
-});
-imgdetector.observe(document.body, {
-  childList: true,
-  subtree: true
+  });
+  imgdetector.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+  console.log("loaded");
 });
